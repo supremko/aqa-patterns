3af4af36516e22e109b9eb2ae0911aa21cc71668
Kirill Lepilov <supremko.fun@gmail.com>
2019-12-21 12:38:04 +0300

    fix #1 update test

--- src/test/java/Delivery.java
+++ src/test/java/Delivery.java
@@ -8,14 +8,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Delivery {
     private LocalDate localDate = LocalDate.now();
     private String formatNow = localDate.format(DateTimeFormatter.ofPattern("dd.MM.YYYY"));
-    private String formatPlusDays = localDate.plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.YYYY"));
+    private String formatPlusDays = localDate.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.YYYY"));
+    private String formatPlusDays2 = localDate.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.YYYY"));
     private SelenideElement form = $("form");
-    private SelenideElement replan = $("[data-test-id='replan-notification']");
     private String url = "http://localhost:9999";
     private Faker faker;
     private String name;
@@ -36,27 +37,29 @@ public class Delivery {
         form.$("[placeholder='Город']").setValue("Санкт-Петербург");
         form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         form.$("[placeholder='Дата встречи']").setValue(formatPlusDays);
-        form.$("[name='name']").setValue("Джон Коннор");
-        form.$("[name='phone']").setValue("+79990000000");
+        form.$("[name='name']").setValue(name);
+        form.$("[name='phone']").setValue(phone);
         form.$("[data-test-id=agreement]").click();
         form.$("[class='button__content']").click();
-//        if (replan.waitUntil(visible,1000).isDisplayed()) {
-            replan.waitUntil(visible,1500).$("[class='button__content']").click();
-//        }
+        $("[data-test-id='success-notification']").waitUntil(visible, 5000);
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        form.$("[placeholder='Дата встречи']").setValue(formatPlusDays2);
+        form.$("[class='button__content']").click();
+        $(byText("Перепланировать")).click();
         $("[data-test-id='success-notification']").waitUntil(visible, 5000);
     }
 
-//    @Test
-//    void checkNegativeDate() {
-//        open(url);
-//        form.$("[placeholder='Город']").setValue("Санкт-Петербург");
-//        form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-//        form.$("[placeholder='Дата встречи']").setValue(formatNow);
-//        form.$("[name='name']").setValue(name);
-//        form.$("[name='phone']").setValue(phone);
-//        form.$("[data-test-id=agreement]").click();
-//        form.$("[class='button__content']").click();
-//        form.$(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
-//    }
+    @Test
+    void checkNegativeDate() {
+        open(url);
+        form.$("[placeholder='Город']").setValue("Санкт-Петербург");
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        form.$("[placeholder='Дата встречи']").setValue(formatNow);
+        form.$("[name='name']").setValue(name);
+        form.$("[name='phone']").setValue(phone);
+        form.$("[data-test-id=agreement]").click();
+        form.$("[class='button__content']").click();
+        form.$(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
+    }
 
 }
